40ff2df52fdc92e7dba1f9ce94def1da02b35a8f
DESKTOP-EV1J3Q9\LENOVO <lixiaona99@gmail.com>
2025-03-05 18:39:58 +0800

    most exceptions for finnbot have been handled, missing exception handler for invalid input that doesn't match any commands

--- src/main/java/Finnbot.java
+++ src/main/java/Finnbot.java
@@ -19,11 +19,13 @@ public class Finnbot {
         System.out.println(line);
     }
 
-//initialise new task object and add to existing array of tasks
+/*initialise new task object and add to existing array of tasks
     public static void addToList(String task) {
         tasks[taskNumber] = new Tasks(task);
         taskNumber++;
     }
+
+ */
 
     public static void addToDos(String task) {
         tasks[taskNumber] = new ToDos(task);
@@ -46,7 +48,7 @@ public class Finnbot {
         }
     }
 
-    public static void markOrUnmarkTask(String responses, boolean isMarked) {
+    public static void taskMarker(String responses, boolean isMarked) {
         try {
             int taskIndex = Integer.parseInt(responses.split(" ")[1]) - 1;
 
@@ -67,16 +69,76 @@ public class Finnbot {
         }
     }
 
+    public static void todoHandler(String response) {
+        try {
+            String description = response.replaceFirst("todo", "").trim();
+            if (description.isEmpty()) {
+                throw new IllegalArgumentException("Todo description is empty, why don't you add something :3");
+            }
+            Tasks todo = new ToDos(description);
+            addToDos(todo.description);
+            System.out.println(todo);
+            System.out.println(line);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Uh oh! The description you have given is invalid!");
+        }
+    }
+
+    public static void deadlineHandler(String response) {
+        try {
+            String description = response.replaceFirst("deadline", "").trim();
+            String[] deadlineParts = description.split("/by");
+            String deadlineDescription = deadlineParts[0];
+            String by = deadlineParts[1];
+            Tasks deadline = new Deadlines(deadlineDescription, by);
+            addDeadline(deadline.description, by);
+            System.out.println(deadline);
+            System.out.println(line);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Deadline format should be: deadline [description] /by [date], why don't you try it again?");
+        }
+    }
+
+    public static void eventHandler(String response) {
+        try {
+            response = response.replaceFirst("events", "");
+            String[] parts = response.split("/from|/to");
+            String eventDescription = parts[0];
+            String start = parts[1];
+            String end = parts[2];
+            Tasks events = new Events(eventDescription, start, end);
+            addEvents(events.description, start, end);
+            System.out.println(events);
+            System.out.println(line);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Event format should be: event [description] /from [start] /to [end] , why don't you try again? :3");
+        }
+    }
+
+    public static void inputValidator (String response) throws EmptyInputException {
+        if (response.trim().isEmpty()) {
+            throw new EmptyInputException("Uh oh, you didn't type anything in so why don't you add something!");
+        }
+    }
+    //problem with default handler
+    public static void defaultHandler(String response) {
+        try {
+            inputValidator(response);
+
+        } catch (EmptyInputException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 
     public static void botRespond() {
         Scanner in = new Scanner(System.in);
-        String addedItem = "added: ";
-        String responses;
+        String response;
 
         while (true) {
-            responses = in.nextLine().trim().toLowerCase();
+            response = in.nextLine().trim().toLowerCase();
             System.out.println(line);
-            String[] splitResponses = responses.split(" ");
+            String[] splitResponses = response.split(" ");
             String command = splitResponses[0].toLowerCase();
 
             switch (command) {
@@ -90,48 +152,29 @@ public class Finnbot {
                 break;
 
             case "mark":
-                markOrUnmarkTask(responses, true);
+                taskMarker(response, true);
                 break;
 
             case "unmark":
-               markOrUnmarkTask(responses, false);
+               taskMarker(response, false);
                 break;
 
             case "todo":
-                responses = responses.replaceFirst("todo", "");
-                Tasks todo = new ToDos(responses);
-                addToDos(todo.description);
-                System.out.println(todo);
-                System.out.println(line);
+                todoHandler(response);
                 break;
 
             case "deadline":
-                responses = responses.replaceFirst("deadline", "");
-                String[] deadlineParts = responses.split("/by");
-                String deadlineDescription = deadlineParts[0];
-                String by = deadlineParts[1];
-                Tasks deadline = new Deadlines(deadlineDescription, by);
-                addDeadline(deadline.description, by);
-                System.out.println(deadline);
-                System.out.println(line);
+                deadlineHandler(response);
                 break;
 
             case "event":
-                responses = responses.replaceFirst("events", "");
-                String[] parts = responses.split("/from|/to");
-                String eventDescription = parts[0];
-                String start = parts[1];
-                String end = parts[2];
-                Tasks events = new Events(eventDescription, start, end);
-                addEvents(events.description, start, end);
-                System.out.println(events);
-                System.out.println(line);
+                eventHandler(response);
                 break;
 
                 default:
-                addToList(responses);
-                System.out.println(addedItem + responses);
-                System.out.println(line);
+                    defaultHandler(response);
+                    break;
+
             }
         }
     }
