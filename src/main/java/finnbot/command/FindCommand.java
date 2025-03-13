1511154fd50cbcb1997788416de777b8a0a82f6c
DESKTOP-EV1J3Q9\LENOVO <lixiaona99@gmail.com>
2025-03-14 03:19:03 +0800

    Added additional java doc comments for FindCommand.java

--- src/main/java/finnbot/command/FindCommand.java
+++ src/main/java/finnbot/command/FindCommand.java
@@ -7,14 +7,32 @@ import finnbot.tasks.Tasks;
 
 import java.util.ArrayList;
 
+/**
+ * The {@code FindCommand} class allows searching for tasks that contain a specific keyword.
+ * It interacts with the {@link TasksList} to find matching tasks and displays them using the UI.
+ */
 public class FindCommand extends Command {
 
     private final String keyword;
 
+    /**
+     * Constructs a {@code FindCommand} with the specified keyword.
+     *
+     * @param keyword The keyword to search for in the task descriptions.
+     */
     public FindCommand(String keyword) {
         this.keyword = keyword;
     }
 
+    /**
+     * Executes the find command, searching for tasks that contain the keyword.
+     * If matching tasks are found, they are displayed using the provided {@code Ui}.
+     * Otherwise, a message indicating no matches are found is displayed.
+     *
+     * @param tasks   The task list to search.
+     * @param ui      The user interface for displaying messages.
+     * @param storage The storage component (not used in this method, but included for consistency).
+     */
     public void execute(TasksList tasks, Ui ui, Storage storage) {
         ArrayList<Tasks> matchingTasks = tasks.keywordFinder(keyword);
 
